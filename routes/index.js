aedc06bbcc0e80abb715fd02c66a94e0297d0ef1
Muckbuck <rosenborg.eric@gmail.com>
2017-02-05 17:03:24 +0100

    make it bett

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 module.exports = function(app){
   
-  app.get('/', function(req,res){
+  app.get('/login', function(req,res){
     var client_id = '3589cfb4-67ae-4bca-aa6a-bf9b45ae371a';
     res.redirect('https://platform.lifelog.sonymobile.com/oauth/2/authorize?client_id=' + client_id + '&scope=lifelog.activities.read');
     console.log(res)
