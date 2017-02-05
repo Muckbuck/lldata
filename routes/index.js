aceab72f1f5a33d5a34ed82a56bb59e31ef1e85d
Muckbuck <rosenborg.eric@gmail.com>
2017-02-05 17:39:15 +0100

    make it bett

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ module.exports = function(app){
   })
   app.get('/login', function(req,res){
     var client_id = '30c5b9a5-cda3-42b5-8340-884cd115042b';
-    res.redirect('https://platform.lifelog.sonymobile.com/oauth/2/authorize?client_id=' + client_id + '&scope=lifelog.activities.read');
+    res.redirect('https://platform.lifelog.sonymobile.com/oauth/2/authorize?client_id=' + client_id + '&scope=lifelog.profile.read+lifelog.activities.read+lifelog.locations.read');
     console.log(res)
     
   });
