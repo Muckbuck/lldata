9fa8b49ca0080947c801cba2bbc784a7dc22ba86
Muckbuck <rosenborg.eric@gmail.com>
2017-02-05 18:04:00 +0100

    make it bett

--- routes/index.js
+++ routes/index.js
@@ -11,35 +11,34 @@ module.exports = function(app){
       method: 'GET',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(postData)
       }
     };
-    var req = http.request(options, (res) => {
-      console.log(`STATUS: ${res.statusCode}`);
-      console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-      res.setEncoding('utf8');
-      res.on('data', (chunk) => {
+    var request = http.request(options, (response) => {
+      console.log(`STATUS: ${response.statusCode}`);
+      console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+      response.setEncoding('utf8');
+      response.on('data', (chunk) => {
         console.log(`BODY: ${chunk}`);
       });
-      res.on('end', () => {
+      response.on('end', () => {
         console.log('No more data in response.');
       });
     });
 
-    req.on('error', (e) => {
+    request.on('error', (e) => {
       console.log(`problem with request: ${e.message}`);
     });
 
     // write data to request body
-    req.write(postData);
-    req.end();
+    request.write(postData);
+    request.end();
     
   })
   app.get('/login', function(req,res){
     var client_id = '30c5b9a5-cda3-42b5-8340-884cd115042b';
     
     res.redirect('https://platform.lifelog.sonymobile.com/oauth/2/authorize?client_id=' + client_id + '&scope=lifelog.profile.read+lifelog.activities.read+lifelog.locations.read');
-    
+    console.log(res)
     
   });
 }
