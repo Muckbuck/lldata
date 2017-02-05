2d59214adaff1dd89b5cf04a7fc23b30e1e26b24
Muckbuck <rosenborg.eric@gmail.com>
2017-02-05 18:16:04 +0100

    make it bett

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,25 @@
 var http = require('http')
 
 module.exports = function(app){
+  var client_id = '30c5b9a5-cda3-42b5-8340-884cd115042b';
+  var client_secret = 'dblEOLzx43KGGALz2t0O9ryHgf8'
   app.get('/', function(req,res){
-    
-    res.render('index', {'code': code})
     var code = req.query.code;
+    var postData = {
+      'client_id': client_id,
+      'client_secret': client_secret,
+      'grant_type': 'refresh_token',
+      'refresh_token': code
+    }
+    res.render('index', {'code': code})
+    
     var options = {
       hostname: 'https://platform.lifelog.sonymobile.com',
       path: '/oauth/2/token',
       method: 'GET',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(postData)
       }
     };
     var request = http.request(options, (response) => {
@@ -30,12 +39,12 @@ module.exports = function(app){
     });
 
     // write data to request body
-    
+    request.write(postData);
     request.end();
     
   })
   app.get('/login', function(req,res){
-    var client_id = '30c5b9a5-cda3-42b5-8340-884cd115042b';
+    
     
     res.redirect('https://platform.lifelog.sonymobile.com/oauth/2/authorize?client_id=' + client_id + '&scope=lifelog.profile.read+lifelog.activities.read+lifelog.locations.read');
     console.log(res)
