ce2507f8745b524cbf5081d3613bd53f37e54815
Muckbuck <rosenborg.eric@gmail.com>
2017-02-05 15:54:59 +0100

    make it better

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var http = require('http')
+var https = require('https')
+var fs = require('fs')
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+require('./routes/index.js')(app);
+
+app.use(express.static('public'))
+
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+const options = {
+  key: fs.readFileSync('keys/key.pem'),
+  cert: fs.readFileSync('keys/cert.pem')
+};
+
+https.createServer(options, app).listen(3000);
---
+++ public/javascripts/app.js
@@ -0,0 +1,19 @@
+var client_id = '3589cfb4-67ae-4bca-aa6a-bf9b45ae371a'
+
+$(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://platform.lifelog.sonymobile.com/oauth/2/authorize?client_id=' + client_id + '&scope=lifelog.activities.read',
+        async: false,
+        
+        contentType: 'application/json',
+        dataType: 'jsonp',
+        success: function (json) {
+            
+            console.log(json)
+        },
+        error: function (e) {
+            console.log(e.message);
+        }
+    });
+});
