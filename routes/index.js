bcb73d500ad6aa995582e868cc00f2827b86617e
Muckbuck <rosenborg.eric@gmail.com>
2017-02-06 10:28:10 +0100

    display access-code

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ module.exports = function(app){
   var client_secret = 'dblEOLzx43KGGALz2t0O9ryHgf8'
   var responseData = null
   app.get('/', function(req,res){
-    var code = req.query.code + '#';
+    var code = req.query.code;
     var postData = {
       'client_id': client_id,
       'client_secret': client_secret,
