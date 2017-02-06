090411e27a86f11ec98abccc7ef4d23d02ad21e8
Muckbuck <rosenborg.eric@gmail.com>
2017-02-06 10:19:49 +0100

    display access-code

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,12 @@ module.exports = function(app){
   var client_secret = 'dblEOLzx43KGGALz2t0O9ryHgf8'
   var responseData = null
   app.get('/', function(req,res){
-    var code = req.query.code;
+    var code = req.query.code + '#';
     var postData = {
       'client_id': client_id,
       'client_secret': client_secret,
       'grant_type': 'authorization_code',
-      'code': code + '#'
+      'code': code
     }
     request.post({url:'https://platform.lifelog.sonymobile.com/oauth/2/refresh_token', form: postData}, function(err,httpResponse,body){ console.log(httpResponse); responseData += JSON.stringify(httpResponse); })
   
