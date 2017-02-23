b833021ead10d495fd05ad3821a0191f7af5a7fd
Muckbuck <rosenborg.eric@gmail.com>
2017-02-23 19:16:32 +0100

    Changed routes

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ app.get('/', function(req,res){
     // Send POST request with auth_code to retrieve access_token
     request.post({url:'https://platform.lifelog.sonymobile.com/oauth/2/token', form: postData}, function(err,httpResponse,body){  responseData = JSON.stringify(httpResponse); });
     var access_token = postData.access_token;
-    console.log(postData);
+    console.log(responseData)
 
     // Send GET request with access_token to retrieve user data    
     var options = {
@@ -49,7 +49,7 @@ app.get('/', function(req,res){
     }
     
     request(options, callback);
-
+    console.log(options);
     res.render('data', {'code': code, 'userData': userData });
     
   })
